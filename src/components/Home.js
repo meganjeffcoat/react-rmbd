54734951a2c765a7519bbbabc6c55642bcd32e91
Megan <jeffcoat.megan@gmail.com>
2020-10-20 02:10:30 -0500

    api fetch and custom hook for fetch on home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE,
+} from "../config";
 
 //import components
-import HeroImage from './elements/HeroImage';
-import SearchBar from './elements/SearchBar';
-import Grid from './elements/Grid';
-import MovieThumb from './elements/MovieThumb';
-import LoadMoreBtn from './elements/LoadMoreBtn';
-import Spinner from './elements/Spinner';
+import HeroImage from "./elements/HeroImage";
+import SearchBar from "./elements/SearchBar";
+import Grid from "./elements/Grid";
+import MovieThumb from "./elements/MovieThumb";
+import LoadMoreBtn from "./elements/LoadMoreBtn";
+import Spinner from "./elements/Spinner";
 
-const Home = () => (
+// custom hook
+import { useHomeFetch } from './hooks/useHomeFetch';
+
+const Home = () => {
+    const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+
+    console.log(state);
+
+  return (
     <>
-        <HeroImage />
-        <SearchBar />
-        <Grid />
-        <MovieThumb />
-        <Spinner />
-        <LoadMoreBtn />
+      <HeroImage />
+      <SearchBar />
+      <Grid />
+      <MovieThumb />
+      <Spinner />
+      <LoadMoreBtn />
     </>
-)
+  );
+};
 
 export default Home;
